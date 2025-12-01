131ac69f4ab44a5efab95682245fb861b0183623
ManuelBanchero <manuelbanchero04@gmail.com>
2025-12-01 15:47:07 -0300

    instructions added

--- day1.ts
+++ day1.ts
@@ -1,3 +1,11 @@
+/* 
+    Santa ha recibido una lista de regalos, pero algunos están defectuosos. 
+    Un regalo es defectuoso si su nombre contiene el carácter #.
+    
+    Ayuda a Santa escribiendo una función que reciba una lista de nombres de regalos y devuelva 
+    una nueva lista que solo contenga los regalos sin defectos.
+*/
+
 function filterGifts(gifts: string[]): string[] {
     return gifts.filter((gift: string): boolean =>
         !gift.includes('#')
